18d6c48fabb0635fd77fd70379616796f9efa065
mapsam <matthews.sam@gmail.com>
2016-06-17 12:15:56 -0700

    adding an async function called "shout"

--- test/hello_world.test.js
+++ test/hello_world.test.js
@@ -1,9 +1,35 @@
 var test = require('tape');
 var HelloWorld = require('../lib/index.js');
+var HW = new HelloWorld();
 
-test('wave hi', function(t) {
-  var HW = new HelloWorld();
+test('wave', function(t) {
   var hello = HW.wave();
-  t.equal(hello, 'howdy world!', 'output of HelloWorld.wave');
+  t.equal(hello, 'howdy world', 'output of HelloWorld.wave');
   t.end();
-})
+});
+
+test('shout', function(t) {
+  HW.shout('rawr', {}, function(err, shout) {
+    if (err) throw err;
+    console.log(shout);
+    t.end();
+  });
+});
+
+test('shout error - non string phrase', function(t) {
+  HW.shout(4, {}, function(err, shout) {
+    t.ok(err);
+    t.end();
+  });
+});
+
+// test('shout error - no options object', function(t) {
+//   HW.shout('rawr', true, function(err, shout) {
+//     t.ok(err);
+//     t.end();
+//   });
+// });
+
+// test('shout error - no callback', function(t) {
+//   t.ok(function() { W.shout('rawr', {}); });
+// });
