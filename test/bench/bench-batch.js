e03dd4cda4d374102a8631e882add6d048a6a4fd
gretacb <carol@mapbox.com>
2016-10-12 14:29:21 -0400

    no need for tape

--- test/bench/bench-batch.js
+++ test/bench/bench-batch.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
-var tape = require('tape');
+var assert = require('assert')
 var HelloWorld = require('../../lib/index.js');
 
 if (!argv.iterations || !argv.concurrency) {
@@ -10,50 +10,42 @@ if (!argv.iterations || !argv.concurrency) {
   process.exit(1);
 }
 
-var opts = { skip: false };
+var HW = new HelloWorld();
+var d3_queue = require('d3-queue');
 
-if (process.env.NPM_FLAGS === '--debug' || process.env.COVERAGE === true) {
-  console.log('# SKIP benchmark: tests are in debug or coverage mode');
-  opts.skip = true;
+var iterations = argv.iterations;
+var concurrency = argv.concurrency;
+var queue = d3_queue.queue(concurrency);
+var runs = 0;
+
+function run(cb) {
+    HW.shout('rawr', {}, function(err, result) {
+      check = result;
+      if (err) {
+        return cb(err);
+      }
+      ++runs;
+      return cb();
+    });
 }
 
-tape('shout batch bench', opts, function(assert) {
-    var HW = new HelloWorld();
-    var d3_queue = require('d3-queue');
+for (var i = 1; i <= iterations; i++) {
+    queue.defer(run);
+}
 
-    var iterations = argv.iterations;
-    var concurrency = argv.concurrency;
-    var queue = d3_queue.queue(concurrency);
-    var runs = 0;
+var time = +(new Date());
 
-    function run(cb) {
-        HW.shout('rawr', {}, function(err, result) {
-          if (err) {
-            return cb(err);
-          }
-          ++runs;
-          return cb();
-        });
-    }
+queue.awaitAll(function(error) {
+  if (error) throw error;
+  if (runs != iterations) {
+    throw new Error("Error: did not run as expected");
+  }
+  // check rate
+  time = +(new Date()) - time;
 
-    for (var i = 1; i <= iterations; i++) {
-        queue.defer(run);
-    }
+  // number of milliseconds per iteration
+  var rate = time/runs;
 
-    var time = +(new Date());
+  assert.equal(rate < 1, true, 'avg time per iteration ( ' + rate + 'ms ) takes less than 1 ms');
 
-    queue.awaitAll(function(error) {
-      if (error) throw error;
-      if (runs != iterations) {
-        throw new Error("Error: did not run as expected");
-      }
-      // check rate
-      time = +(new Date()) - time;
-
-      // number of milliseconds per iteration
-      var rate = time/runs;
-
-      assert.equal(rate < 1, true, 'avg time per iteration ( ' + rate + 'ms ) takes less than 1 ms');
-      assert.end();
-    });
 });
