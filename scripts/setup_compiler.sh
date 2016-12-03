d4459235f68f00595b1d6931a3fb2c4e38c28054
Dane Springmeyer <dane@mapbox.com>
2016-12-02 16:39:42 -0800

    upgrade to llvm 3.9.0

--- scripts/setup_compiler.sh
+++ scripts/setup_compiler.sh
@@ -4,7 +4,7 @@ set -eu
 set -o pipefail
 
 if [[ $(uname -s) == 'Linux' ]]; then
-    mason install clang 3.8.0
-    export PATH=$(mason prefix clang 3.8.0)/bin:${PATH}
+    mason install clang++ 3.9.0
+    export PATH=$(mason prefix clang++ 3.9.0)/bin:${PATH}
     which clang++
 fi
