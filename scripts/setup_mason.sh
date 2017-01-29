111143281a9e398d221fa6f3a8080a6b0f19a3ee
Dane Springmeyer <dane@mapbox.com>
2017-01-29 08:02:34 -0800

    unbreak rvm - get mason path right

--- scripts/setup_mason.sh
+++ scripts/setup_mason.sh
@@ -9,8 +9,8 @@ function setup_mason() {
  # install clang++ via mason
   rm -rf /tmp/mason-${MASON_RELEASE}
   mkdir /tmp/mason-${MASON_RELEASE}
-  curl -sSfL https://github.com/mapbox/mason/archive/v${MASON_RELEASE}.tar.gz | tar --gunzip --extract --strip-components=1 --directory=/tmp/mason
-  export PATH=/tmp/mason:${PATH}
+  curl -sSfL https://github.com/mapbox/mason/archive/v${MASON_RELEASE}.tar.gz | tar --gunzip --extract --strip-components=1 --directory=/tmp/mason-${MASON_RELEASE}
+  export PATH=/tmp/mason-${MASON_RELEASE}:${PATH}
 }
 
 setup_mason
