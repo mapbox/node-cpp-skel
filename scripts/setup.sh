8d542c008b8a533ee30f2160b4dc429b849298ee
Dane Springmeyer <dane@mapbox.com>
2017-01-29 11:48:31 -0800

    rework setup method to avoid needing to source a script that might error

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,27 +3,84 @@
 set -eu
 set -o pipefail
 
-export MASON_VERSION="0.5.0"
-export CLANG_VERSION="3.9.1"
+export MASON_RELEASE="${MASON_RELEASE:-0.5.0}"
+export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-3.9.1}"
 
-# unbreak bash shell due to rvm bug on osx: https://github.com/direnv/direnv/issues/210#issuecomment-203383459
-# this impacts any usage of scripts that are source'd (like this one)
-if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-  echo 'shell_session_update() { :; }' > ~/.direnvrc
+function run() {
+    local config=${1}
+    # unbreak bash shell due to rvm bug on osx: https://github.com/direnv/direnv/issues/210#issuecomment-203383459
+    # this impacts any usage of scripts that are source'd (like this one)
+    if [[ "${TRAVIS_OS_NAME:-}" == "osx" ]]; then
+      echo 'shell_session_update() { :; }' > ~/.direnvrc
+    fi
+
+    #
+    # MASON
+    #
+
+    function setup_mason() {
+      local install_dir=${1}
+      local mason_release=${2}
+      if [[ ! -d ${install_dir} ]]; then
+          mkdir -p ${install_dir}
+          curl -sSfL https://github.com/mapbox/mason/archive/v${mason_release}.tar.gz | tar --gunzip --extract --strip-components=1 --directory=${install_dir}
+      fi
+    }
+
+    setup_mason $(pwd)/.mason ${MASON_RELEASE}
+
+    #
+    # COMPILER
+    #
+
+    .mason/mason install clang++ ${MASON_LLVM_RELEASE}
+    .mason/mason link clang++ ${MASON_LLVM_RELEASE}
+
+    echo "export PATH=$(pwd)/.mason:$(pwd)/mason_packages/.link/bin:"'${PATH}' > ${config}
+    echo "export CXX=$(pwd)/mason_packages/.link/bin/clang++" >> ${config}
+    echo "export MASON_RELEASE=${MASON_RELEASE}" >> ${config}
+    echo "export MASON_LLVM_RELEASE=${MASON_LLVM_RELEASE}" >> ${config}
+    RT_BASE=$(pwd)/mason_packages/.link/lib/clang/${MASON_LLVM_RELEASE}/lib/$(uname | tr A-Z a-z)/libclang_rt
+    if [[ $(uname -s) == 'Darwin' ]]; then
+        RT_PRELOAD=${RT_BASE}.asan_osx_dynamic.dylib
+    else
+        RT_PRELOAD=${RT_BASE}.asan-x86_64.so
+    fi
+    echo "export MASON_LLVM_RT_PRELOAD=${RT_PRELOAD}" >> ${config}
+
+    exit 0
+}
+
+function usage() {
+  >&2 echo "Usage"
+  >&2 echo ""
+  >&2 echo "$ ./scripts/setup.sh --config local.env"
+  >&2 echo "$ source local.env"
+  >&2 echo ""
+  exit 1
+}
+
+if [[ ! ${1:-} ]]; then
+  usage
 fi
 
-#
-# MASON
-#
-
-source ./scripts/setup_mason.sh ${MASON_VERSION}
-
-#
-# COMPILER
-#
-
-source ./scripts/setup_compiler.sh ${CLANG_VERSION}
-
-# set back to non-strict bash mode to avoid breaking travis itself
-set +eu
-set +o pipefail
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+for i in "$@"
+do
+case $i in
+    --config)
+    if [[ ! ${2:-} ]]; then
+      usage
+    fi
+    shift
+    run $@
+    ;;
+    -h | --help)
+    usage
+    shift
+    ;;
+    *)
+    usage
+    ;;
+esac
+done
--- scripts/setup_compiler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-set -o pipefail
-
-CLANG_VERSION="${1}"
-mason install clang++ ${CLANG_VERSION}
-export PATH=$(mason prefix clang++ ${CLANG_VERSION})/bin:${PATH}
-export CXX=$(mason prefix clang++ ${CLANG_VERSION})/bin/clang++
