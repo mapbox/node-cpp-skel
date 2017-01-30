251ab9b91de3d10c606264195232dd9ee3e37e57
Dane Springmeyer <dane@mapbox.com>
2017-01-29 23:00:13 -0800

    build sanitizer setup into setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -48,6 +48,16 @@ function run() {
         RT_PRELOAD=${RT_BASE}.asan-x86_64.so
     fi
     echo "export MASON_LLVM_RT_PRELOAD=${RT_PRELOAD}" >> ${config}
+    SUPPRESSION_FILE="/tmp/leak_suppressions.txt"
+    echo "leak:__strdup" > ${SUPPRESSION_FILE}
+    echo "leak:v8::internal" >> ${SUPPRESSION_FILE}
+    echo "leak:node::CreateEnvironment" >> ${SUPPRESSION_FILE}
+    echo "leak:node::Init" >> ${SUPPRESSION_FILE}
+    echo "export LSAN_OPTIONS=suppressions=${SUPPRESSION_FILE}" >> ${config}
+    echo "export ASAN_OPTIONS=check_initialization_order=1:detect_stack_use_after_return=1" >> ${config}
+    echo "export MASON_SANITIZE=address,integer,alignment,bounds,float-cast-overflow,float-divide-by-zero,integer-divide-by-zero,nonnull-attribute,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,unreachable,unsigned-integer-overflow,vla-bound" >> ${config}
+    echo 'export MASON_SANITIZE_CXXFLAGS="-fsanitize=${MASON_SANITIZE} -fsanitize-address-use-after-scope -fno-omit-frame-pointer -fno-common"' >> ${config}
+    echo 'export MASON_SANITIZE_LDFLAGS="-fsanitize=${MASON_SANITIZE}"' >> ${config}
 
     exit 0
 }
