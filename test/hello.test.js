bc4ea2c13f3e2f792244cf519ccca6a21e9dc4c8
gretacb <carol@mapbox.com>
2017-06-14 19:13:02 -0500

    why doesnt travis like the hello function?

--- test/hello.test.js
+++ test/hello.test.js
@@ -2,6 +2,7 @@ var test = require('tape');
 var module = require('../lib/index.js');
 
 test('prints world', function(t) {
+  console.log(module);
   var check = module.hello();
   t.equal(check, 'world', 'returned world');
   t.end();
