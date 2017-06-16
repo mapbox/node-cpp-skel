5e6d34c4a11ce161bb6dc86270d78066512abe9f
gretacb <carol@mapbox.com>
2017-06-16 11:47:41 -0500

    bump coverage of error cases

--- test/hello_async.test.js
+++ test/hello_async.test.js
@@ -17,10 +17,28 @@ test('success: prints regular busy world', function(t) {
   });
 });
 
-test('fail: handles invalid arg value', function(t) {
+test('fail: handles invalid louder value', function(t) {
   module.hello_async({ louder: 'oops' }, function(err, result) {
     t.ok(err, 'expected error');
     t.ok(err.message.indexOf('option \'louder\' must be a boolean') > -1, 'expected error message');
     t.end();
   });
+});
+
+test('fail: handles invalid options value', function(t) {
+  module.hello_async('oops', function(err, result) {
+    t.ok(err, 'expected error');
+    t.ok(err.message.indexOf('first arg \'options\' must be an object') > -1, 'expected error message');
+    t.end();
+  });
+});
+
+test('fail: handles missing callback', function(t) {
+  try {
+    module.hello_async({ louder: 'oops' }, {});
+  } catch (err) {
+    t.ok(err, 'expected error');
+    t.ok(err.message.indexOf('callback') > -1, 'proper error message');
+    t.end();
+  }
 });
