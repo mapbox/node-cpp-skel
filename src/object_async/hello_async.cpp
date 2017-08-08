4df31d7a9ec5b8d25a806f6fe4eb603db1164244
gretacb <carol@mapbox.com>
2017-08-08 16:22:09 -0500

    remove travis flags, add memory bug

--- src/object_async/hello_async.cpp
+++ src/object_async/hello_async.cpp
@@ -124,6 +124,12 @@ std::string do_expensive_work(bool louder, std::string const& name)
     for (std::size_t i = 0; i < work_to_do; ++i)
     {
         std::string const& item = container[i];
+
+        char *x = (char*)malloc(10 * sizeof(char*));
+        free(x);
+        int y = x[5];
+        y += 2;
+
         if (item != std::to_string(i))
         {
 
@@ -182,7 +188,7 @@ struct AsyncHelloWorker : Nan::AsyncWorker
     // - Finally, you call the user's callback with your results
     void HandleOKCallback() override
     {
-        //Nan::HandleScope scope;
+        Nan::HandleScope scope;
 
         const auto argc = 2u;
         v8::Local<v8::Value> argv[argc] = {
@@ -275,7 +281,7 @@ void HelloObjectAsync::Init(v8::Local<v8::Object> target)
     // Fun trivia: forgetting a handlescope is one of the most common causes of
     // memory leaks in node.js core
     // https://www.joyent.com/blog/walmart-node-js-memory-leak
-    //Nan::HandleScope scope;
+    Nan::HandleScope scope;
 
     // This is saying:
     // "Node, please allocate a new Javascript string object
