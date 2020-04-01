6d08efe1bfc4f12fd11115168082cf98e1f9e3f6
Artem Pavlenko <artem@mapnik.org>
2020-04-01 12:43:27 +0100

    Return std::unique_ptr<std::vector<char>> and stop abusing std::string::data with const_cast (clang-tidy)

--- src/cpu_intensive_task.hpp
+++ src/cpu_intensive_task.hpp
@@ -3,17 +3,20 @@
 #include <chrono>
 #include <string>
 #include <thread>
+#include <vector>
 
 namespace detail {
 
 // simulate CPU intensive task
-inline std::unique_ptr<std::string> do_expensive_work(std::string const& name, bool louder)
+inline std::unique_ptr<std::vector<char>> do_expensive_work(std::string const& name, bool louder)
 {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    std::unique_ptr<std::string> result = std::make_unique<std::string>("...threads are busy async bees...hello " + name);
+    std::string str = "...threads are busy async bees...hello " + name;
+    std::unique_ptr<std::vector<char>> result = std::make_unique<std::vector<char>>(str.begin(), str.end());
     if (louder)
     {
-        *result += "!!!!";
+        std::string extra{"!!!!"};
+        std::copy(extra.c_str(), extra.c_str() + extra.length(), back_inserter(*result));
     }
     return result;
 }
